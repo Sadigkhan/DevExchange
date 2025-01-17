db078072a1b98cfce5518aa3552b6348b889009d
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-01-18 01:12:23 +0400

    Light&Dard mode bug fixed.Shadcn navbar ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,29 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import React from "react";
+
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/navigation/navbar";
+import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
-  weight: "100 200 300 400 500 600 700 800 900",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
 const spaceGrotesk = localFont({
   src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
-  weight: "300 400 500 600 700 800 900",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
-  title: "DevExchange",
+  title: "DevFlow",
   description:
-    "Təcrübəli proqramçıların və yeni başlayanların bir araya gəldiyi, suallarınızı verə və cavabları paylaşa biləcəyiniz bir platforma! Proqramlaşdırma suallarınıza cavab tapın, biliyi paylaşın və dünya üzrə inkişaf etdiricilər ilə əməkdaşlıq edin. Web inkişafı, mobil tətbiq inkişafı, alqoritmlər, verilənlər strukturları və daha çox mövzularda dərinləşin. Həmçinin, bu platforma, proqramlaşdırma biliklərinizi inkişaf etdirmək və problemləri sürətli həll etmək üçün ideal yerdir.",
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
-    icon: "./imagessite-logo.svg",
+    icon: "/images/site-logo.svg",
   },
 };
 
