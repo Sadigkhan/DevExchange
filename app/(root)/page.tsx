25bceaf41c72703f56cb2afecffd6929812da290
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-01-18 23:23:53 +0400

    Github authentication completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
+import { auth, signOut } from "@/auth";
+import ROUTES from "@/constants/routes";
 import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const session = await auth();
+  console.log(session)
   return (
     <>
-     
+      <h1 className="h1-bold">Welcome to the world of NextJs</h1>
+
+      <form  className="px-10 pt-[100px]"  action={async ()=>{
+        "use server"
+
+        await signOut({redirectTo:ROUTES.SIGN_IN});
+      }}>
+        <button type="submit">Log Out</button>
+      </form>
     </>
   );
 };
