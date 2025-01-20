05eaabd5c2b3fb297c4535261c311ff3d260c3df
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2025-01-21 01:21:48 +0400

    Home page structure created. Next will be local search implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,32 @@
 // import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 import React from "react";
 
 const Home = async () => {
   // const session = await auth();
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of NextJs</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Question</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask A Question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-11">LocalSearch</section>
+      Home Filter
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question card 1</p>
+        <p>Question card 1</p>
+        <p>Question card 1</p>
+        <p>Question card 1</p>
+      </div>
     </>
   );
 };
